4b213a54bfdf52a917036f2e0aa0a20bfd21efff
danilomassoni <massonidanilo@gmail.com>
2025-02-01 14:42:22 -0300

    Correção de exibição de gráfico para visualização.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = dash.Dash(__name__)
 app.title = "Previsão Climática - São Paulo"
 
 # Carregar os dados históricos usando a classe DataLoader
-data_loader = DataLoader('data\processed\dataframe_formatado.csv')
+data_loader = DataLoader(r'data\processed\dataframe_formatado.csv')
 df_historico = data_loader.load_data()
 
 
@@ -19,7 +19,7 @@ app.layout = html.Div([
     html.H1("Previsão do Clima em São Paulo", style={'textAlign': 'center'}),
     html.Div([
         html.Label("Quantos anos deseja prever?"),
-        dcc.Input(id="yaers_input", type="number", value=5, min=1, max=20, step=1),
+        dcc.Input(id="years_input", type="number", value=5, min=1, max=20, step=1),
         html.Button("Atualizar Previsão", id="submit_button", n_clicks=0)
     ], style={'margin': '20px'}),
 
@@ -35,7 +35,7 @@ app.layout = html.Div([
 
 def update_forecast(n_clicks, years_to_predict):
     if not years_to_predict or years_to_predict < 1:
-        return {}
+        return {'data': [], 'layout': go.Layout(title="Previsão de Temperatura", xaxis_title="Ano", yaxis_title="Temperatura (ºC)")}
 
     # Criar e treinar o modelo
     climate_model = ClimateModel(df_historico)
@@ -49,7 +49,8 @@ def update_forecast(n_clicks, years_to_predict):
         go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], mode='lines', name='Limite Superior', line=dict(dash='dot'))
     ]
 
-    return {'data': data, 'layout': go.Layout(title="PRevisão de temperatura", xaxix_title="Ano", yaxis_title="Temperatura (ºC)")}
+    return {'data': data, 'layout': go.Layout(title="Previsão de Temperatura", xaxis_title="Ano", yaxis_title="Temperatura (ºC)")}
+
 
 
 # Rodar o servidor 
